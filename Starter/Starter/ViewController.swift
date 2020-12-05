82389672281e054e744a6a194614cb57a5a8dc0d
Aung Ko Ko Thet <aungkoko.thet@mm-digital-solutions.com>
2020-12-05 11:19:53 +0630

    init project

---
+++ Starter/Starter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Starter
+//
+//  Created by Aung Ko Ko Thet on 05/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
