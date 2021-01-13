b5ab1f028e4ed3b8e07ed1aee6abca2bf0623719
Aung Ko Ko Thet <aungkoko.thet@mm-digital-solutions.com>
2021-01-14 03:06:55 +0630

    -Understanding UITableView

--- Starter/Starter/ViewController.swift
+++ Starter/Starter/ViewController.swift
@@ -9,22 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var textFieldName: UITextField!
-    @IBOutlet weak var labelName: UILabel!
-    
-    @IBOutlet weak var imageProfile: UIImageView!
-    
-    @IBAction func didTapButton(_ sender: Any) {
-        labelName.text = textFieldName.text
-        textFieldName.text = ""
-    }
+    @IBOutlet weak var tableViewContent: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let tapGestureForImage = UITapGestureRecognizer(target: self, action: #selector(onTapImage))
-        imageProfile.addGestureRecognizer(tapGestureForImage)
-        imageProfile.isUserInteractionEnabled = true
+        tableViewContent.dataSource = self
+        tableViewContent.register(UINib(nibName: String(describing: Sample1TableViewCell.self), bundle: nil), forCellReuseIdentifier: String(describing: Sample1TableViewCell.self))
+        tableViewContent.register(UINib(nibName: String(describing: Sample2TableViewCell.self), bundle: nil), forCellReuseIdentifier: String(describing: Sample2TableViewCell.self))
         
         debugPrint("viewDidLoad")
     }
@@ -48,11 +40,36 @@ class ViewController: UIViewController {
         super.viewDidAppear(animated)
         debugPrint("viewDidDisappear")
     }
-    
-    @objc func onTapImage(){
-        textFieldName.text = "Aung Ko Ko Thet"
-    }
-
 
 }
 
+extension ViewController : UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        switch indexPath.section {
+        case 0:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: Sample1TableViewCell.self), for: indexPath) as? Sample1TableViewCell else{
+                return UITableViewCell()
+            }
+            return cell
+        case 1:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: Sample2TableViewCell.self), for: indexPath) as? Sample2TableViewCell else {
+                return UITableViewCell()
+            }
+            return cell
+        default:
+            return UITableViewCell()
+        }
+        
+    }
+    
+}
