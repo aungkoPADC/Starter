6c0799d23734c4041f9a79e3c8cf498290169deb
Aung Ko Ko Thet <aungkoko.thet@mm-digital-solutions.com>
2021-01-14 02:00:05 +0630

    -Storyboard , ViewController , Connecting UI with ViewController and ViewController Life Cycle

---
+++ Starter/Starter/MovieDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MovieDetailViewController.swift
+//  Starter
+//
+//  Created by Aung Ko Ko Thet on 14/01/2021.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- Starter/Starter/ViewController.swift
+++ Starter/Starter/ViewController.swift
@@ -9,10 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var labelName: UILabel!
+    
+    @IBOutlet weak var imageProfile: UIImageView!
+    
+    @IBAction func didTapButton(_ sender: Any) {
+        labelName.text = textFieldName.text
+        textFieldName.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        main()
+        
+        let tapGestureForImage = UITapGestureRecognizer(target: self, action: #selector(onTapImage))
+        imageProfile.addGestureRecognizer(tapGestureForImage)
+        imageProfile.isUserInteractionEnabled = true
+        
+        debugPrint("viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        debugPrint("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        debugPrint("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        debugPrint("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        debugPrint("viewDidDisappear")
+    }
+    
+    @objc func onTapImage(){
+        textFieldName.text = "Aung Ko Ko Thet"
     }
 
 
