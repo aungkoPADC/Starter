592ba978f126dea705316a69fab2b572348f10df
Aung Ko Ko Thet <aungkoko.thet@mm-digital-solutions.com>
2021-01-29 04:10:45 +0630

    -Movie ShowCases Collectionview cell

---
+++ Starter/Starter/cells/collectionview/swifts/ShowCaseCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ShowCaseCollectionViewCell.swift
+//  Starter
+//
+//  Created by Aung Ko Ko Thet on 29/01/2021.
+//
+
+import UIKit
+
+class ShowCaseCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
