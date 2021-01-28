29a4c6f3d2b82f8ef50ed27bfcd0a7adfbea782c
Aung Ko Ko Thet <aungkoko.thet@mm-digital-solutions.com>
2021-01-28 09:29:00 +0630

    -Genre Movie List collection view

--- Starter/Starter/cells/tableview/swifts/GenreTableViewCell.swift
+++ Starter/Starter/cells/tableview/swifts/GenreTableViewCell.swift
@@ -13,6 +13,7 @@ class GenreTableViewCell: UITableViewCell {
     @IBOutlet weak var collectionViewMovie: UICollectionView!
     
     let genreList = [GenreVO(name: "ACTION", isSelected: true),GenreVO(name: "DRAMA", isSelected: false),GenreVO(name: "COMEDY", isSelected: false),GenreVO(name: "ADVENTUREEEEEEE", isSelected: false),GenreVO(name: "BIOGRAPHY", isSelected: false)]
+    let movieList = [""]
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -21,6 +22,11 @@ class GenreTableViewCell: UITableViewCell {
         collectionViewGenre.delegate = self
         
         collectionViewGenre.register(UINib(nibName: String(describing: GenreCollectionViewCell.self), bundle: nil), forCellWithReuseIdentifier: String(describing: GenreCollectionViewCell.self))
+        
+        collectionViewMovie.dataSource = self
+        collectionViewMovie.delegate = self
+        
+        collectionViewMovie.register(UINib(nibName: String(describing: PopularFilmCollectionViewCell.self), bundle: nil), forCellWithReuseIdentifier: String(describing: PopularFilmCollectionViewCell.self))
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -34,28 +40,45 @@ class GenreTableViewCell: UITableViewCell {
 extension GenreTableViewCell : UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == collectionViewMovie {
+            return 10
+        }
         return genreList.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: GenreCollectionViewCell.self), for: indexPath) as? GenreCollectionViewCell else {
-            return UICollectionViewCell()
-        }
-        cell.data = genreList[indexPath.row]
-        cell.onTapItem = { genreName in
-            self.genreList.forEach { (genreVO) in
-                if genreName == genreVO.name{
-                    genreVO.isSelected = true
-                }else{
-                    genreVO.isSelected = false
-                }
+        if collectionView == collectionViewMovie {
+            
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: PopularFilmCollectionViewCell.self), for: indexPath) as? PopularFilmCollectionViewCell else {
+                return UICollectionViewCell()
             }
-            self.collectionViewGenre.reloadData()
+            return cell
+            
+        }else{
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: GenreCollectionViewCell.self), for: indexPath) as? GenreCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            cell.data = genreList[indexPath.row]
+            cell.onTapItem = { genreName in
+                self.genreList.forEach { (genreVO) in
+                    if genreName == genreVO.name{
+                        genreVO.isSelected = true
+                    }else{
+                        genreVO.isSelected = false
+                    }
+                }
+                self.collectionViewGenre.reloadData()
+                self.collectionViewMovie.reloadData()
+                
+            }
+            return cell
         }
-        return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if collectionView == collectionViewMovie {
+            return CGSize(width: collectionView.frame.width/3, height: CGFloat(225))
+        }
         return CGSize(width: widthOfString(text: genreList[indexPath.row].name,font: UIFont(name: "Geeza Pro Regular", size: 14) ?? UIFont.systemFont(ofSize: 14))+20, height: 45)
     }
     
