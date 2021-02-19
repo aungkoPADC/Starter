235a3a07c6dfe47b04ee1a10b4bca5af9615488b
Aung Ko Ko Thet <aungkoko.thet@mm-digital-solutions.com>
2021-02-20 06:14:46 +0630

    -Movie Detail (Part-1)

--- Starter/Starter/viewcontrollers/MovieDetailViewController.swift
+++ Starter/Starter/viewcontrollers/MovieDetailViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class MovieDetailViewController: UIViewController {
 
+    @IBOutlet weak var ivBack: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        initGestureRecognizers()
 
         // Do any additional setup after loading the view.
     }
     
+    private func initGestureRecognizers(){
+        let tapGestureForBack = UITapGestureRecognizer(target: self, action: #selector(onTapBack))
+        ivBack.isUserInteractionEnabled = true
+        ivBack.addGestureRecognizer(tapGestureForBack)
+    }
+    
 
+    @objc func onTapBack(){
+        self.dismiss(animated: true, completion: nil)
+    }
 }
