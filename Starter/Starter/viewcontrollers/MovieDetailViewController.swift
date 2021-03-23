4688d61a278e2ce2a9ec8056d6126fa3876f10a9
Aung Ko Ko Thet <aungkoko.thet@mm-digital-solutions.com>
2021-03-23 20:39:50 +0630

    Movie Detail (Part-2)

--- Starter/Starter/viewcontrollers/MovieDetailViewController.swift
+++ Starter/Starter/viewcontrollers/MovieDetailViewController.swift
@@ -10,12 +10,25 @@ import UIKit
 class MovieDetailViewController: UIViewController {
 
     @IBOutlet weak var ivBack: UIImageView!
+    @IBOutlet weak var btnRateMovie: UIButton!
+    @IBOutlet weak var collectionViewActors: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        registerCollectionViewCells()
         initGestureRecognizers()
 
+        btnRateMovie.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        btnRateMovie.layer.borderWidth = 2
+        btnRateMovie.layer.cornerRadius = 20
         // Do any additional setup after loading the view.
+    }
+    
+    private func registerCollectionViewCells(){
+        collectionViewActors.delegate = self
+        collectionViewActors.dataSource = self
+        collectionViewActors.register(UINib(nibName: String(describing: ActorCollectionViewCell
+                                                                .self), bundle: nil), forCellWithReuseIdentifier: String(describing: ActorCollectionViewCell.self))
     }
     
     private func initGestureRecognizers(){
@@ -28,4 +41,25 @@ class MovieDetailViewController: UIViewController {
     @objc func onTapBack(){
         self.dismiss(animated: true, completion: nil)
     }
+}
+
+extension MovieDetailViewController : UICollectionViewDataSource , UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: ActorCollectionViewCell.self), for: indexPath) as? ActorCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/2.5, height: CGFloat(200))
+    }
+    
+    
+    
 }
